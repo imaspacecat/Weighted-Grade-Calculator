5449ad097a89db4891377a0613ca29257b467125
yotam <yotam@snow>
2021-10-09 21:13:38 -0700

    Completely reworked the code and added a GUI with Javafx

--- Main.java
+++ Main.java
@@ -1,57 +1,78 @@
-import java.util.Scanner;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
 
-class Main {
-  public static int size = 100;
-  public static Subject[] subject = new Subject[size];
-  public static int index = 0;
-  public static int userSize = 0;
 
-  public static void main(String[] args) {
-  
 
-    boolean running = false;
+public class Main extends Application{
+	String[] stringInput;
+	Subject[] subject = new Subject[10];
+	int i=0;
+	int userSize=0;
+  	public static void main(String[] args) {
+  		Application.launch(args);
 
-    Scanner scanner = new Scanner(System.in);
+  	}
 
-    running = true;
-    
+@Override
+	public void start(Stage primaryStage) throws Exception {
+		  
+		primaryStage.setTitle("Weighted Grade Calculator");
+		
+		Text text1 = new Text();
+		Text instructions = new Text("Category, Weight (%), Grade (%) \n \n E.g. homework, 30, 98");
+		Text solution = new Text();
+		
+		TextField textField1 = new TextField();
+		textField1.setText("homework, 30, 90");
+		
+		Button button1 = new Button("Enter");
+		Button button2 = new Button("Calculate");
+		
+		
+		VBox vBox1 = new VBox(instructions, textField1, button1, button2, text1, solution);
+		
+		Scene scene1 = new Scene(vBox1);
+		
+		primaryStage.setScene(scene1);
+		
+		primaryStage.show();
+				
+		
+		
+		button1.setOnAction(action -> {
+            text1.setText("Latest Input: " + textField1.getText());
+            stringInput = textField1.getText().split(",", 0);
+            subject[i]= new Subject(stringInput[0], Double.parseDouble(stringInput[1]), Double.parseDouble(stringInput[2]));
+            userSize++;
+            i++;
+        });
+		
+		button2.setOnAction(action -> {
+            solution.setText("Grade: " + Double.toString(calculate(subject)) + "%");
+        });
 
-    while(running){
+	  }
 
-      System.out.println("Enter the category name, grade weight, and the grade in the format (x, y, z). Leave it blank and press enter to calculate");
 
-      String input = scanner.nextLine();
+	double calculate(Subject[] input){
+		double grade = 0;
+		for(int i=0; i<userSize; i++) {
+			grade = grade + input[i].getWeight() * input[i].getGrade();
+		}
+		
+		return grade/100;
+	}
+	
+	void getInput() {
+		
+	}
+	
 
-      if(input.equals("")){
-        running = false;
-        calculate();
-        break;
-      }
-      
-      String[] splitArray = input.split(",", 0);
-    
-      subject[index] = new Subject(splitArray[0], Double.parseDouble(splitArray[1]), Double.parseDouble(splitArray[2]));
-     
-      index = index + 1;
-      userSize = userSize + 1;
-    }
 
-  }
-
-  public static void calculate(){
-    double average;
-    double nominator = 0;
-    double denominator = 0;
-
-    for(int i = 0; i < userSize; i++){
-      nominator = nominator + subject[i].getWeight() * subject[i].getGrade();
-      denominator = denominator + subject[i].getWeight();
-    }
-
-    average = nominator / denominator;
-
-    System.out.println("Grade is: " + average);
-
-  }
-  
+	
 }
--- Subject.java
+++ Subject.java
@@ -1,32 +1,32 @@
 class Subject{
+	String inputCat;
+	double inputWeight;
+	double inputGrade;
+	  
 
-  String inputCat;
-  double inputWeight;
-  double inputGrade;
+
+	public Subject(String inputCat, double inputWeight, double inputGrade){
+		this.inputCat = inputCat;
+	    this.inputWeight = inputWeight;
+	    this.inputGrade = inputGrade;
+
+	  }
+
+	void print(){
+	    System.out.println(inputCat + " " + inputWeight + " " + inputGrade);
+	  }
+
+	String getCat(){
+	    return inputCat;
+	  }
+
+	double getWeight(){
+	    return inputWeight;
+	  }
+
+	double getGrade(){
+	    return inputGrade;
+	  }
+	  
+	}
   
-
-
-  public Subject(String inputCat, double inputWeight, double inputGrade){
-    this.inputCat = inputCat;
-    this.inputWeight = inputWeight;
-    this.inputGrade = inputGrade;
-
-  }
-
-  public void print(){
-    System.out.println(inputCat + " " + inputWeight + " " + inputGrade);
-  }
-
-  public String getCat(){
-    return inputCat;
-  }
-
-  public double getWeight(){
-    return inputWeight;
-  }
-
-  public double getGrade(){
-    return inputGrade;
-  }
-  
-}
